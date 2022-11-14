3d6fe8b9e7e6784af7d187cd0f5a6832835c202f
AlesandMacBook <a.tabs@bk.ru>
2022-11-14 19:57:42 +0400

    Выполнение работ

---
+++ lesson-5/Program.cs
@@ -0,0 +1,133 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int size = 10;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int countEvenNumbers = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0) countEvenNumbers ++;
+}
+Console.Write(countEvenNumbers);
+
+
+// Функции
+
+// Функция заполнения массива случайными трехзначными числами
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+// Функция вывода массива в терминал
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+
+int size = 4;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int sumNumbersEvenIndex = 0;
+
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    sumNumbersEvenIndex += numbers[i];
+}
+Console.Write(sumNumbersEvenIndex);
+
+
+// Функции
+
+// Функция заполнения массива случайными числами от -100 до 100
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+// Функция вывода массива в терминал
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+[3 7 22 2 78] -> 76
+*/
+
+int size = 10;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int max = numbers[0];
+int min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+Console.WriteLine($"Минимальное число: {min}");
+Console.WriteLine($"Минимальное число: {max}");
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+
+// Функции
+
+// Функция заполнения массива случайными числами от 1 до 554
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 555);
+    }
+}
+
+// Функция вывода массива в терминал
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
---
+++ lesson-7/Program.cs
@@ -0,0 +1,132 @@
+﻿// Задача 47. Двумерный массив размером m×n, заполненный случайными вещественными числами.
+void Task47()
+{
+    Console.WriteLine("Введите количество строк:");
+    int linesVol = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов:");
+    int columnsVol = Convert.ToInt32(Console.ReadLine());
+    double[,] numbers = new double[linesVol, columnsVol];
+    FillArrayRandomNumbers(numbers);
+    PrintArray(numbers);
+
+    void FillArrayRandomNumbers(double[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            }
+        }
+    }
+
+    void PrintArray(double[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write("[ ");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+    }
+}
+
+// Задача 50. Программа, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+void Task50()
+{
+
+    Console.WriteLine("Введите номер строкиЖ:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите номер столбца:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int[,] numbers = new int[10, 10];
+    FillArrayRandomNumbers(numbers);
+
+    if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+    {
+        Console.WriteLine("такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n - 1, m - 1]}");
+    }
+
+    PrintArray(numbers);
+
+    void FillArrayRandomNumbers(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = new Random().Next(-100, 100) / 10;
+            }
+        }
+    }
+
+    void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write("[ ");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+    }
+}
+
+// Задача 52.Задайте двумерный массив из целых чисел.Найдите среднее арифметическое элементов в каждом столбце.
+                //Например, задан массив:
+                //1 4 7 2
+                //5 9 2 3
+                //8 4 2 4
+                //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.]
+                Random random = new Random();
+                int rows = random.Next(3, 4);
+                int columns = random.Next(3, 4);
+                int[,] array = new int[rows, columns];
+                FillArray(array);
+                PrintArray(array);
+                void FillArray(int[,] ar, int startNumber = 0, int finishNumber = 10)
+                {
+                    finishNumber++;
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < columns; j++)
+                        {
+                            array[i, j] = random.Next(startNumber, finishNumber);
+                        }
+                    }
+                }
+                void PrintArray(int[,] ar)
+                {
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < columns; j++)
+                        {
+                            Console.Write(array[i, j] + "   ");
+                        }
+                        Console.WriteLine();
+                    }
+                    for (int i = 0; i < rows; i++)
+                    {
+                        double Average = 0;
+                        for (int j = 0; j < columns; j++)
+                        {
+                            Average += array[j, i];
+                        }
+                        Console.Write($"\nCреднее арифметическое:");
+                        Average = Math.Round(Average / columns, 1);
+                        Console.WriteLine($"столбца № {i + 1} {"="} {Average}");
+
+                    }
+                    
